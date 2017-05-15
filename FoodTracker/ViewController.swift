198e4a3d33eed0fa1f0988026f71943f0c221188
Akiko Michibayashi <am@Akikos-MacBook-Pro.local>
2017-05-14 19:12:38 -0700

    converting FoodTracker dir to a git repo

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Akiko Michibayashi on 5/12/17.
+//  Copyright © 2017 Akiko Michibayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
